0b3949163884a836a69fecbccfbb603c354c649c
Sonny Sheth <sonny.sheth@gmail.com>
2016-02-10 22:23:02 -0200

    add line to app.configure

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.configure(function() {
     app.use(partials());
     app.use(express.bodyParser());
     app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/dist'));
     app.use(express.cookieParser('shhhh, very secret'));
     app.use(express.session());
 });
